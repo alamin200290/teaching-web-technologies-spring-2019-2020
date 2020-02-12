70f1810109d8dd528ae5466aca0130f67277c11c
Md.Al-Amin <amin.se.aiub@gmail.com>
2020-02-12 16:58:21 +0600

    php intro

---
+++ PHP/abc.php
@@ -0,0 +1,81 @@
+<?php
+
+	echo $_GET['a'];
+
+//print_r($_GET);
+
+/*	$a = 10;
+	$b = 6;
+	function sum(){
+		global $a;
+		global $b;
+
+		return $a+$b;
+		//return $GLOBALS['a']+$GLOBALS['b'];
+	}*/
+
+	//echo sum();
+
+
+/*
+	$xyz = "value from PHP";
+
+	if($a === $b){
+		echo "true";
+	}else{
+		echo "false";
+	}*/
+/*	define("GREETING", "Welcome to W3Schools.com!");
+	echo GREETING;*/
+
+	//phpinfo();
+
+	//var_dump($a);
+	//$val = print($a);
+	//echo "<br>". $val;
+
+	//$abc = [4];
+	//$xyz = array(4);
+	//$student = ['name'=>'alamin', 'id'=> '121111-11'];
+	//echo $abc[0];
+
+/*	$students = [ 
+					['alamin', 'webtech', 3.6], 
+					['Xyz', 'Data Structure', 3.0], 
+					['abc', 'atp-3', 2.1], 
+					['pgr', 'ICS', 1.5] 
+				];
+
+*/
+/*	$students = [ 
+					's1'=> ['name'=>'alamin', 'course'=> 'webtech', 'cgpa'=>3.6], 
+					's2'=> ['name'=>'Xyz', 'course'=> 'Data Structure', 'cgpa'=>3.0], 
+					's3'=> ['name'=>'abc', 'course'=> 'atp-3', 'cgpa'=>2.1], 
+					's4'=> ['name'=>'pgr', 'course'=> 'ICS', 'cgpa'=>1.5] 
+				];*/
+	//echo $students['s4']['course'];
+
+
+/*	function sum($a=0, $b=0){
+		return $a+$b;
+	}
+
+	for($i=0; $i<5;){
+		 $i++;
+		//header('content-type: text/plain');
+		echo $i.". webtech"."<br>";
+	}*/
+
+?>
+
+
+<!-- <!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>This is html content:     <?php print($xyz); ?> </h1>
+</body>
+</html>
+ -->
