3bd4f839f70b5e7664c39520ae51131e0a1d515d
Md.Al-Amin <amin.se.aiub@gmail.com>
2020-03-11 17:00:20 +0600

    image upload and show

--- fileupload/file_upload.php
+++ fileupload/file_upload.php
@@ -1,16 +1,17 @@
 <?php
-	
+	session_start();
 	if(isset($_POST['submit'])){
 
 		//echo $_FILES['mypic']['name']."<br>";
 		//echo $_FILES['mypic']['size'];
 
 		$filename = $_FILES['mypic']['name'];
-		$dest = "upload/"."abc.png";
+		$dest = "upload/".$filename;
 		$src = $_FILES['mypic']['tmp_name'];
 
 		if(move_uploaded_file($src, $dest)){
-			echo " File uploaded at :".$dest;
+			$_SESSION['pic'] = $filename;
+			header('location: home.php');
 		}else{
 			echo "Error";
 		}
---
+++ fileupload/home.php
@@ -0,0 +1,14 @@
+<?php
+
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+		<img src="upload/<?=$_SESSION['pic']?>">
+</body>
+</html>
